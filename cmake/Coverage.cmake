77a26339c1a7a9f66c2605aec2fe38447620a675
Bronek Kozicki <brok@incorrekt.com>
2024-01-07 19:37:34 +0000

    Add coverage reporting, small updates in README (#5)
    * Add coverage reporting
    * Fix dummy gcov errors
    * Update README.md

---
+++ cmake/Coverage.cmake
@@ -0,0 +1,28 @@
+# Enable coverate report target and compilation options
+
+# In this project, 100% tests coverage does not actually mean much, because the most useful
+# test cases are around overload resolution, parameter dispatch, value categories,
+# concepts etc. elements of the language which may not show in the coverage tests.
+# This means that we need many more tests than just the minimum needed to ensure good coverage.
+
+set(CODE_COVERAGE_VERBOSE ON)
+set(CODE_COVERAGE_FORMAT "xml" CACHE STRING "Format of the coverage report.")
+set(CODE_COVERAGE_TEST "ctest" CACHE STRING "Command to run tests.")
+set(CODE_COVERAGE_TEST_ARGS "--output-on-failure" CACHE STRING "Parameters to the test command.")
+set(GCOVR_ADDITIONAL_ARGS
+--exclude-noncode-lines
+--exclude-unreachable-branches
+--exclude-throw-branches
+--print-summary
+--gcov-ignore-errors=no_working_dir_found)
+
+setup_target_for_coverage_gcovr(
+    NAME coverage
+    FORMAT ${CODE_COVERAGE_FORMAT}
+    EXECUTABLE ${CODE_COVERAGE_TEST}
+    EXECUTABLE_ARGS ${CODE_COVERAGE_TEST_ARGS}
+    EXCLUDE "tests" "external" "src"
+    DEPENDENCIES tests examples
+)
+
+append_coverage_compiler_flags()
