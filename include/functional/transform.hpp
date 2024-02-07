34da4a21ca700ea0d22996430fdcd76ff06ce032
Bronek Kozicki <brok@incorrekt.com>
2024-02-07 15:22:58 +0000

    Rename `closure` to `pack` and refactor (#28)
    * Remove useles #include detail/concepts.hpp
    * Refactor closure, rename to pack, add concept

--- include/functional/transform.hpp
+++ include/functional/transform.hpp
@@ -7,7 +7,6 @@
 #define INCLUDE_FUNCTIONAL_TRANSFORM
 
 #include "functional/concepts.hpp"
-#include "functional/detail/concepts.hpp"
 #include "functional/functor.hpp"
 #include "functional/fwd.hpp"
 #include "functional/utility.hpp"
