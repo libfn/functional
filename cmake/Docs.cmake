7cedc9c85e2deeccdfee32693bd22713be796ec0
Alex Kremer <akremer@ripple.com>
2024-02-22 14:04:32 +0000

    Add cmake target to export Znai docs

---
+++ cmake/Docs.cmake
@@ -0,0 +1,27 @@
+find_package(Doxygen REQUIRED)
+find_package(Znai REQUIRED)
+
+set(DOXYGEN_GENERATE_HTML NO)
+set(DOXYGEN_GENERATE_XML YES)
+
+macro(znai_export_docs TARGET SOURCE_DIR DEPLOY_DIR)
+  add_custom_target(${TARGET}
+    COMMAND ${Znai} --source ${SOURCE_DIR} --deploy ${DEPLOY_DIR} --doc-id functional
+    COMMENT "Exporting documentation to ${DEPLOY_DIR}"
+  )
+endmacro()
+
+doxygen_add_docs(
+    docs
+    ${CMAKE_CURRENT_SOURCE_DIR}/include
+    COMMENT "Generate documentation"
+)
+
+znai_export_docs(
+  export_docs
+  ${CMAKE_CURRENT_SOURCE_DIR}/docs
+  ${CMAKE_BINARY_DIR}/docs
+  COMMENT "Export final documentation"
+  )
+  
+  add_dependencies(export_docs docs)
