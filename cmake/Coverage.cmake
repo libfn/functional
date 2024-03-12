ebf4534db770a5f56d7c371971a26ce2734a01a3
Bronek Kozicki <brok@incorrekt.com>
2024-03-12 16:13:14 +0000

    Switch coverage to unit tests only (#43)

--- cmake/Coverage.cmake
+++ cmake/Coverage.cmake
@@ -7,8 +7,8 @@
 
 set(CODE_COVERAGE_VERBOSE ON)
 set(CODE_COVERAGE_FORMAT "xml" CACHE STRING "Format of the coverage report.")
-set(CODE_COVERAGE_TEST "ctest" CACHE STRING "Command to run tests.")
-set(CODE_COVERAGE_TEST_ARGS "--output-on-failure" CACHE STRING "Parameters to the test command.")
+set(CODE_COVERAGE_TEST "$<TARGET_FILE:tests>" CACHE STRING "Command to run tests.")
+set(CODE_COVERAGE_TEST_ARGS "-a" CACHE STRING "Parameters to the test command.")
 set(GCOVR_ADDITIONAL_ARGS
 --exclude-noncode-lines
 --exclude-unreachable-branches
@@ -22,7 +22,7 @@ setup_target_for_coverage_gcovr(
     EXECUTABLE ${CODE_COVERAGE_TEST}
     EXECUTABLE_ARGS ${CODE_COVERAGE_TEST_ARGS}
     EXCLUDE "tests" "external"
-    DEPENDENCIES tests examples
+    DEPENDENCIES tests
 )
 
 append_coverage_compiler_flags()
