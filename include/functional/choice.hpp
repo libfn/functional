1f63267636e3b324c310ad47a89d25e7a51e34d1
Bronek Kozicki <brok@incorrekt.com>
2024-04-13 19:12:02 +0100

    Remove coverage exclude markers

--- include/functional/choice.hpp
+++ include/functional/choice.hpp
@@ -259,7 +259,7 @@ template <typename... Ts, typename... Tx>
 {
   return lh.template transform_to<bool>([&rh]<typename T>(std::in_place_type_t<T> d, auto const &lh) noexcept {
     if constexpr (std::remove_cvref_t<decltype(rh)>::template has_type<T>) {
-      return rh.has_value(d) && lh == *rh.get_ptr(d); // GCOVR_EXCL_BR_LINE
+      return rh.has_value(d) && lh == *rh.get_ptr(d);
     } else {
       return false;
     }
