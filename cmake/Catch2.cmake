624be77c203edbb689bf1dd1369fd93a0a085c55
Bronek Kozicki <brok@incorrekt.com>
2024-04-17 12:47:54 +0100

    Update license scan to include CMake dependencies

--- cmake/Catch2.cmake
+++ cmake/Catch2.cmake
@@ -1,9 +1,11 @@
 include(FetchContent)
+
 FetchContent_Declare(
   Catch2
   GIT_SHALLOW    TRUE
   GIT_REPOSITORY https://github.com/catchorg/Catch2.git
   GIT_TAG        v3.5.4)
+
 FetchContent_MakeAvailable(Catch2)
 
 list(APPEND CMAKE_MODULE_PATH ${Catch2_SOURCE_DIR}/extras)
