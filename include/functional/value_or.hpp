a0d58f545ca763ff125f688be55f27b1e0c4a9fb
Bronek Kozicki <brok@incorrekt.com>
2024-04-30 18:47:48 +0100

    Add value_or functor

---
+++ include/functional/value_or.hpp
@@ -0,0 +1,44 @@
+// Copyright (c) 2024 Bronek Kozicki
+//
+// Distributed under the ISC License. See accompanying file LICENSE.md
+// or copy at https://opensource.org/licenses/ISC
+
+#ifndef INCLUDE_FUNCTIONAL_OR_ELSE
+#define INCLUDE_FUNCTIONAL_OR_ELSE
+
+#include "functional/concepts.hpp"
+#include "functional/functor.hpp"
+
+#include <type_traits>
+
+namespace fn {
+template <typename V, typename... Args>
+concept invocable_value_or //
+    = (some_expected_non_void<V> && std::is_constructible_v<typename std::remove_cvref_t<V>::value_type, Args...>)
+      || (some_optional<V> && std::is_constructible_v<typename std::remove_cvref_t<V>::value_type, Args...>);
+
+constexpr inline struct value_or_t final {
+  [[nodiscard]] constexpr auto operator()(auto &&...args) const noexcept -> functor<value_or_t, decltype(args)...> //
+  {
+    return {FWD(args)...};
+  }
+
+  struct apply;
+} value_or = {};
+
+struct value_or_t::apply final {
+  [[nodiscard]] static constexpr auto operator()(some_monadic_type auto &&v, auto &&...args) noexcept //
+      -> same_value_kind<decltype(v)> auto
+    requires invocable_value_or<decltype(v), decltype(args)...>
+  {
+    using type = std::remove_cvref_t<decltype(v)>;
+    return FWD(v).or_else([&](auto &&...) -> type { return type{std::in_place, args...}; });
+  }
+
+  // No support for choice since there's no error to recover from
+  static auto operator()(some_choice auto &&v, auto &&...args) noexcept = delete;
+};
+
+} // namespace fn
+
+#endif // INCLUDE_FUNCTIONAL_OR_ELSE
