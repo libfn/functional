4e75eb9423712fa14299ee176c2393a99a8e0df7
Bronek Kozicki <brok@incorrekt.com>
2024-10-13 15:52:49 +0100

    Switch to default runners (#91)
    * Switch to default runners
    * Update actions
    * Remove wretry.action
    * Switch to libfn containers
    * Remove tests/main
    * Add CMAKE_FIND_CATCH2 to cmake

--- cmake/Catch2.cmake
+++ cmake/Catch2.cmake
@@ -1,13 +1,18 @@
 if(DISABLE_FETCH_CONTENT)
   find_package(Catch2 3)
-else()  
-  include(FetchContent)
-  FetchContent_Declare(
-    Catch2
-    GIT_SHALLOW    TRUE
-    GIT_REPOSITORY https://github.com/catchorg/Catch2.git
-    GIT_TAG        v3.7.1)
-  FetchContent_MakeAvailable(Catch2)
-  
-  list(APPEND CMAKE_MODULE_PATH ${Catch2_SOURCE_DIR}/extras)
+else()
+  if(DEFINED ENV{CMAKE_FIND_CATCH2})
+    find_package(Catch2 3)
+  else()
+    find_package(Catch2 3)
+    include(FetchContent)
+    FetchContent_Declare(
+      Catch2
+      GIT_SHALLOW    TRUE
+      GIT_REPOSITORY https://github.com/catchorg/Catch2.git
+      GIT_TAG        v3.7.1)
+    FetchContent_MakeAvailable(Catch2)
+
+    list(APPEND CMAKE_MODULE_PATH ${Catch2_SOURCE_DIR}/extras)
+  endif()
 endif()
