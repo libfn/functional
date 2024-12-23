af23c6196043988ecd325516370e710b88e7b6c7
Bronek Kozicki <brok@incorrekt.com>
2024-12-23 16:22:05 +0000

    Rename docs target, remove C++ options

--- cmake/Catch2.cmake
+++ cmake/Catch2.cmake
@@ -1,13 +1,15 @@
-if(DEFINED ENV{CMAKE_FIND_CATCH2} OR DISABLE_FETCH_CONTENT)
-  find_package(Catch2 3)
-else()
-  include(FetchContent)
-  FetchContent_Declare(
-    Catch2
-    GIT_SHALLOW    TRUE
-    GIT_REPOSITORY https://github.com/catchorg/Catch2.git
-    GIT_TAG        v3.7.1)
-  FetchContent_MakeAvailable(Catch2)
+# Pull or find Catch2 dependency for unit tests
 
-  list(APPEND CMAKE_MODULE_PATH ${Catch2_SOURCE_DIR}/extras)
+if((DEFINED ENV{CMAKE_FIND_CATCH2}) OR DISABLE_FETCH_CONTENT)
+    find_package(Catch2 3)
+else()
+    include(FetchContent)
+    FetchContent_Declare(
+        Catch2
+        GIT_SHALLOW    TRUE
+        GIT_REPOSITORY https://github.com/catchorg/Catch2.git
+        GIT_TAG        v3.7.1)
+    FetchContent_MakeAvailable(Catch2)
+
+    list(APPEND CMAKE_MODULE_PATH ${Catch2_SOURCE_DIR}/extras)
 endif()
--- cmake/Docs.cmake
+++ cmake/Docs.cmake
@@ -5,23 +5,24 @@ set(DOXYGEN_GENERATE_HTML NO)
 set(DOXYGEN_GENERATE_XML YES)
 
 macro(znai_export_docs TARGET SOURCE_DIR DEPLOY_DIR)
-add_custom_target(${TARGET}
-COMMAND ${Znai} --source ${SOURCE_DIR} --deploy ${DEPLOY_DIR} --doc-id '""' --lookup-paths ${CMAKE_BINARY_DIR}
-COMMENT "Exporting documentation to ${DEPLOY_DIR}"
-  )
+    add_custom_target(
+        ${TARGET}
+        COMMAND ${Znai} --source ${SOURCE_DIR} --deploy ${DEPLOY_DIR} --doc-id '""' --lookup-paths ${CMAKE_BINARY_DIR}
+        COMMENT "Exporting documentation to ${DEPLOY_DIR}"
+    )
 endmacro()
 
 doxygen_add_docs(
-    docs
+    docs_xml
     ${CMAKE_CURRENT_SOURCE_DIR}/include
     COMMENT "Generate documentation"
 )
 
 znai_export_docs(
-  export_docs
-  ${CMAKE_CURRENT_SOURCE_DIR}/docs
-  ${CMAKE_BINARY_DIR}/docs
-  COMMENT "Export final documentation"
+    export_docs
+    ${CMAKE_CURRENT_SOURCE_DIR}/docs
+    ${CMAKE_BINARY_DIR}/docs
+    COMMENT "Export final documentation"
 )
 
-add_dependencies(export_docs docs)
+add_dependencies(export_docs docs_xml)
