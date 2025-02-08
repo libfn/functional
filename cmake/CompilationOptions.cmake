7222f55d01dac7c5a6f80800b06c959c3248f10b
Bronek Kozicki <brok@incorrekt.com>
2025-02-08 13:45:13 +0000

    Compilation options cleanup

--- cmake/CompilationOptions.cmake
+++ cmake/CompilationOptions.cmake
@@ -1,29 +1,46 @@
 # Add compilation options appropriate for the current compiler
 
+# Note, we do not select libc++ like an example below. Instead the user should
+# use the CXXFLAGS environment variable for this option.
+#
+# if(CMAKE_CXX_COMPILER_ID MATCHES "Clang" AND (NOT APPLE))
+#     add_compile_options(-stdlib=libc++)
+#     add_link_options(-lc++)
+# endif()
+
+
 function(append_compilation_options)
-    set(options WARNINGS OPTIMIZATION)
-    set(oneValueArgs NAME)
-    cmake_parse_arguments(Options "${options}" "${oneValueArgs}" "" ${ARGN})
+    set(options WARNINGS OPTIMIZATION INTERFACE)
+    set(Options_NAME ${ARGV0})
+    cmake_parse_arguments(Options "${options}" "" "" ${ARGN})
 
     if(NOT DEFINED Options_NAME)
-      message(FATAL_ERROR "NAME must be set")
+        message(FATAL_ERROR "NAME must be set")
     endif()
 
     if(CMAKE_CXX_COMPILER_ID MATCHES "MSVC")
-      if(Options_WARNINGS)
-          target_compile_options(${Options_NAME} PRIVATE /W4)
-      endif()
+        if(Options_WARNINGS)
+            target_compile_options(${Options_NAME} PRIVATE /W4)
+        endif()
 
-      if(Options_OPTIMIZATION)
-          target_compile_options(${Options_NAME} PRIVATE $<IF:$<CONFIG:Debug>,/Od,/Ox>)
-      endif()
-    else()
-      if(Options_WARNINGS)
-          target_compile_options(${Options_NAME} PRIVATE -Wall -Wextra -Wpedantic)
-      endif()
+        if(Options_OPTIMIZATION)
+            target_compile_options(${Options_NAME} PRIVATE $<IF:$<CONFIG:Debug>,/Od,/Ox>)
+        endif()
+    elseif(CMAKE_CXX_COMPILER_ID MATCHES "GNU" OR CMAKE_CXX_COMPILER_ID MATCHES "(Apple)?Clang")
+        if(Options_WARNINGS)
+            target_compile_options(${Options_NAME} PRIVATE -Wall -Wextra -Wpedantic -Werror)
+        endif()
 
-      if(Options_OPTIMIZATION)
-          target_compile_options(${Options_NAME} PRIVATE $<IF:$<CONFIG:Debug>,-O0,-O2>)
-      endif()
+        if(Options_OPTIMIZATION)
+            target_compile_options(${Options_NAME} PRIVATE $<IF:$<CONFIG:Debug>,-O0,-O2>)
+        endif()
+
+        if(Options_INTERFACE)
+            if(CMAKE_CXX_COMPILER_ID MATCHES "GNU")
+                target_compile_options(${Options_NAME} INTERFACE -Wno-non-template-friend)
+            endif()
+
+            target_compile_options(${Options_NAME} INTERFACE -Wno-missing-braces)
+        endif()
     endif()
 endfunction()
