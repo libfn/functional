261b8ed2588f524f408325e6262a39eaa2bd2912
Bronek Kozicki <brok@incorrekt.com>
2025-03-02 17:46:32 +0000

    Remove static state from test helper

--- tests/util/helper_types.hpp
+++ tests/util/helper_types.hpp
@@ -3,6 +3,7 @@
 // Distributed under the ISC License. See accompanying file LICENSE.md
 // or copy at https://opensource.org/licenses/ISC
 
+#include <compare>
 #include <concepts>
 #include <initializer_list>
 #include <stdexcept>
@@ -10,7 +11,6 @@
 #include <utility>
 
 template <auto V> struct helper_t {
-  static inline int witness = 0;
   int v = {};
 
   // Use prime numbers to record Foo states in witness
@@ -25,64 +25,63 @@ template <auto V> struct helper_t {
   // No default constructor
   helper_t() = delete;
 
-  ~helper_t() noexcept = default;
+  constexpr ~helper_t() noexcept = default;
 
-  bool operator==(helper_t const &) const noexcept = default;
+  constexpr bool operator==(helper_t const &) const noexcept = default;
 
   // Assignment operators will multiply witness by a prime
-  helper_t &operator=(helper_t &o) noexcept
+  constexpr helper_t &operator=(helper_t &o) noexcept
   {
     v = o.v;
-    witness *= from_lval;
+    v *= from_lval;
     return *this;
   }
 
-  helper_t &operator=(helper_t const &o) noexcept
+  constexpr helper_t &operator=(helper_t const &o) noexcept
   {
     v = o.v;
-    witness *= from_lval_const;
+    v *= from_lval_const;
     return *this;
   }
 
-  helper_t &operator=(helper_t &&o) noexcept
+  constexpr helper_t &operator=(helper_t &&o) noexcept
   {
     v = o.v;
-    witness *= from_rval;
+    v *= from_rval;
     return *this;
   }
 
-  helper_t &operator=(helper_t const &&o) noexcept
+  constexpr helper_t &operator=(helper_t const &&o) noexcept
   {
     v = o.v;
-    witness *= from_rval_const;
+    v *= from_rval_const;
     return *this;
   }
 
-  // Every constructor will increase witness, including copy/move
-  helper_t(helper_t &&o) noexcept : v(o.v) { witness *= from_rval; }
-  helper_t(helper_t const &o) noexcept : v(o.v) { witness *= from_lval_const; }
+  constexpr helper_t(helper_t &o) noexcept : v(o.v) { v *= from_lval; }
+  constexpr helper_t(helper_t const &o) noexcept : v(o.v) { v *= from_lval_const; }
+  constexpr helper_t(helper_t &&o) noexcept : v(o.v) { v *= from_rval; }
+  constexpr helper_t(helper_t const &&o) noexcept : v(o.v) { v *= from_rval_const; }
 
-  helper_t(std::integral auto... a) noexcept
+  constexpr helper_t(std::integral auto... a) noexcept
     requires(sizeof...(a) > 0) // intentionally implicit when sizeof...(a) == 1
       : v((1 * ... * a))
   {
-    witness += v;
   }
 
   template <typename T> //
-  explicit helper_t(std::integral_constant<T, V>) noexcept : v(static_cast<int>(T::value))
+  constexpr explicit helper_t(std::integral_constant<T, V>) noexcept : v(static_cast<int>(T::value))
   {
-    witness += v;
   }
 
   // Potentially throwing constructor
-  helper_t(std::initializer_list<double> list, std::integral auto... a) noexcept(false) : v(init(list, a...)) //
+  constexpr helper_t(std::initializer_list<double> list, std::integral auto... a) noexcept(false)
+      : v(init(list, a...)) //
   {
-    witness += v;
   }
 
   // ... and the actual exception being thrown
-  static int init(std::initializer_list<double> l, auto &&...a) noexcept(false)
+  static constexpr int init(std::initializer_list<double> l, auto &&...a) noexcept(false)
   {
     double ret = (1 * ... * a);
     for (auto d : l) {
@@ -92,13 +91,18 @@ template <auto V> struct helper_t {
     }
     return static_cast<int>(ret);
   }
+
+  // Disable comparison operators; compare .v instead
+  friend bool operator==(helper_t, std::integral auto) = delete;
+  friend std::strong_ordering operator<=>(helper_t, helper_t) = delete;
 };
 
 // Swap will also multiply witness by a prime
-template <auto V> void swap(helper_t<V> &l, helper_t<V> &r)
+template <auto V> constexpr void swap(helper_t<V> &l, helper_t<V> &r)
 {
   std::swap(l.v, r.v);
-  helper_t<V>::witness *= helper_t<V>::swapped;
+  l.v *= l.swapped;
+  r.v *= r.swapped;
 }
 
 using helper = helper_t<0>;
