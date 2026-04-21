9fbe8e4c74e522aa62b25cc79fade755d58304d2
Bronek Kozicki <brok@incorrekt.com>
2026-04-21 18:27:36 +0000

    Refactoring of polygon example

--- examples/polygon/main.cpp
+++ examples/polygon/main.cpp
@@ -6,28 +6,34 @@
 #include <filesystem>
 #include <fstream>
 #include <iostream>
-#include <numeric>
+#include <istream>
+#include <list>
 #include <set>
 #include <string>
 #include <type_traits>
 #include <utility>
-#include <vector>
 
 struct parameters {
   std::string characters;
-  std::vector<std::string> files;
+  std::list<std::string> files;
 
-  struct too_few_parameters {};
+  struct too_few_parameters {
+    std::string const message;
+    static constexpr int code = 1;
+  };
+
   struct too_few_characters {
-    static constexpr char const *message = "Error: at least 3 characters must be provided";
+    static constexpr char const message[] = "Error: at least 3 characters must be provided";
+    static constexpr int code = 2;
   };
 
   using error = fn::sum<too_few_characters, too_few_parameters>;
 
-  static auto make(int argc, char *argv[]) -> fn::expected<parameters, error>
+  static auto make(int argc, char const *argv[]) -> fn::expected<parameters, error>
   {
-    if (argc < 3) {
-      return std::unexpected(too_few_parameters{});
+    if (argc < 2) {
+      return std::unexpected(
+          too_few_parameters{.message = "Usage: " + std::string(argv[0]) + " <characters> [<file>...]"});
     }
 
     parameters params;
@@ -41,18 +47,12 @@ struct parameters {
     }
     return params;
   }
-
-  static void print_error(auto &&error, char const *const program_name)
-  {
-    if constexpr (std::same_as<std::remove_cvref_t<decltype(error)>, too_few_parameters>) {
-      std::cerr << "Usage: " << program_name << " <characters> <file>...\n";
-    } else
-      std::cerr << error.message << "\n";
-  }
 };
 
 template <typename T>
 concept parameters_error = parameters::error::has_type<std::remove_cvref_t<T>>;
+
+void print_error(parameters_error auto &&err) { std::cerr << err.message << "\n"; }
 
 using counter = std::array<std::size_t, 256>;
 
@@ -71,38 +71,47 @@ struct inputs {
   };
 
   struct file_not_found : file_error {
-    static constexpr char const *message = "Error: file not found";
+    static constexpr char const message[] = "Error: file not found";
+    static constexpr int code = 3;
   };
   struct permission_denied : file_error {
-    static constexpr char const *message = "Error: permission denied";
+    static constexpr char const message[] = "Error: permission denied";
+    static constexpr int code = 4;
   };
 
   using error = fn::sum<file_not_found, permission_denied>;
 
   counter characters;
   unsigned char required_character;
-  std::vector<std::ifstream> files;
+  std::list<std::unique_ptr<std::ifstream>> files;
+  std::list<std::istream *> inputs; // Yes, this name is legal here and I will not change it
 
-  static auto make(parameters &&p) -> fn::expected<inputs, error>
+  static auto make(parameters &&p) -> fn::expected<struct inputs, error>
   {
-    static constexpr auto open_file = [](std::string const &filename) -> fn::expected<std::ifstream, error> {
+    struct inputs result {
+      .characters = count_characters(p.characters), .required_character = static_cast<unsigned char>(p.characters[0]),
+      .files = {}, .inputs = {},
+    };
+
+    if (p.files.empty()) {
+      result.inputs.push_back(&std::cin);
+      return result;
+    }
+
+    static constexpr auto open_file
+        = [](std::string const &filename) -> fn::expected<std::unique_ptr<std::ifstream>, error> {
       std::filesystem::path path(filename);
       if (!std::filesystem::exists(path)) {
         return std::unexpected(file_not_found{{.path = std::move(path)}});
       }
 
-      std::ifstream file(path);
-      if (!file.is_open()) {
+      auto file = std::make_unique<std::ifstream>(path);
+      if (!file->is_open()) {
         return std::unexpected(permission_denied{{.path = std::move(path)}});
       }
       return std::move(file);
     };
 
-    inputs result{.characters = count_characters(p.characters),
-                  .required_character = static_cast<unsigned char>(p.characters[0]),
-                  .files = {}};
-
-    // Cannot use accumulate here because ifstream disables copy assignment
     for (auto const &filename : p.files) {
       auto file = open_file(filename);
 
@@ -111,64 +120,68 @@ struct inputs {
         return std::unexpected(std::move(file).error());
 
       result.files.push_back(std::move(file).value());
+      result.inputs.push_back(result.files.back().get());
     }
 
     return result;
   }
-
-  static void print_error(auto &&error) { std::cerr << error.message << ": " << error.path << "\n"; }
 };
 
 template <typename T>
 concept inputs_error = inputs::error::has_type<std::remove_cvref_t<T>>;
 
-int main(int argc, char *argv[])
-{
-  static constexpr auto wrap = [](auto &&v) -> fn::expected<std::remove_cvref_t<decltype(v)>, fn::sum<>> { //
-    return FWD(v);
-  };
+void print_error(inputs_error auto &&err) { std::cerr << err.message << ": " << err.path << "\n"; }
 
-  static constexpr auto match = [](counter const &lh, counter const &rh) -> bool {
-    for (std::size_t i = 0; i < rh.size(); ++i) {
-      if (lh[i] < rh[i]) {
-        return false;
+constexpr inline struct algorithm_t {
+  auto operator()(inputs const &inputs) const -> int
+  {
+    std::set<std::string> words;
+
+    constexpr auto match = [](counter const &lh, counter const &rh) -> bool {
+      for (std::size_t i = 0; i < rh.size(); ++i) {
+        if (lh[i] < rh[i]) {
+          return false;
+        }
+      }
+      return true;
+    };
+
+    for (std::istream *input : inputs.inputs) {
+      std::string line;
+      while (std::getline(*input, line)) {
+        auto const counts = count_characters(line);
+
+        if (line.size() >= 3 && counts[inputs.required_character] > 0 && match(inputs.characters, counts)) {
+          if (words.insert(line).second) {
+            if (inputs.characters == counts)
+              std::cout << "* ";
+            std::cout << line << "\n";
+          }
+        }
       }
     }
-    return true;
+
+    return 0;
+  }
+} algorithm;
+
+auto main(int argc, char const **argv) -> int
+try {
+  static constexpr auto wrap = [](auto &&v) -> fn::expected<std::remove_cvref_t<decltype(v)>, fn::sum<>> { //
+    return FWD(v);
   };
 
   return ((wrap(argc) & wrap(argv))        //
           | fn::and_then(parameters::make) //
           | fn::and_then(inputs::make)     //
-          | fn::transform([](inputs &&i) -> int {
-              std::set<std::string> words;
-
-              return std::accumulate( //
-                  i.files.begin(), i.files.end(),
-                  0, //
-                  [&i, &words](int, std::ifstream &file) -> int {
-                    std::string line;
-                    while (std::getline(file, line)) {
-                      auto counts = count_characters(line);
-                      if (line.size() >= 3 && counts[i.required_character] > 0 && match(i.characters, counts)) {
-                        if (words.insert(line).second) {
-                          if (i.characters == counts)
-                            std::cout << "* ";
-                          std::cout << line << "\n";
-                        }
-                      }
-                    }
-                    return 0;
-                  });
-            })
-          | fn::or_else( //
-              fn::overload{[argv](parameters_error auto &&p) -> fn::expected<int, fn::sum<>> {
-                             parameters::print_error(p, argv[0]);
-                             return 1;
-                           },
-                           [](inputs_error auto &&e) -> fn::expected<int, fn::sum<>> {
-                             inputs::print_error(e);
-                             return 2;
-                           }}))
+          | fn::transform(algorithm)       //
+          | fn::or_else(                   //
+              [](auto &&p) -> fn::expected<int, fn::sum<>> {
+                print_error(p);
+                return p.code;
+              }))
       .value();
+} catch (std::exception const &e) {
+  std::cerr << e.what() << '\n';
+  return 13;
 }
