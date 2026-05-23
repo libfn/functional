6c040245c5b2cdc60f1166225e2935a8aaf240f5
Bronek Kozicki <brok@incorrekt.com>
2026-05-23 21:30:43 +0200

    More options cleanup and fixes

--- cmake/CompilationOptions.cmake
+++ cmake/CompilationOptions.cmake
@@ -1,21 +1,21 @@
 # Add compilation options appropriate for the current compiler
-
-# Note, we do not select libc++ like an example below. Instead the user should
-# use the CXXFLAGS environment variable for this option.
 #
-# if(CMAKE_CXX_COMPILER_ID MATCHES "Clang" AND (NOT APPLE))
-#     add_compile_options(-stdlib=libc++)
-#     add_link_options(-lc++)
-# endif()
-
+# The user is expected to select libc++ via the CXXFLAGS environment variable
+# rather than from this file.
 
 function(append_compilation_options)
-    set(options WARNINGS OPTIMIZATION INTERFACE TESTS)
+    if(ARGC EQUAL 0)
+        message(FATAL_ERROR "target name must be provided as the first positional argument")
+    endif()
     set(Options_NAME ${ARGV0})
-    cmake_parse_arguments(Options "${options}" "" "" ${ARGN})
+    set(_rest ${ARGN})
+    list(REMOVE_AT _rest 0)
 
-    if(NOT DEFINED Options_NAME)
-        message(FATAL_ERROR "NAME must be set")
+    set(options WARNINGS OPTIMIZATION INTERFACE TESTS)
+    cmake_parse_arguments(Options "${options}" "" "" ${_rest})
+
+    if(Options_UNPARSED_ARGUMENTS)
+        message(FATAL_ERROR "Unknown options: ${Options_UNPARSED_ARGUMENTS}")
     endif()
 
     if(CMAKE_CXX_COMPILER_ID MATCHES "MSVC")
@@ -23,11 +23,11 @@ function(append_compilation_options)
             # disable C4456: declaration of 'b' hides previous local declaration
             # disable C4244: 'initializing': conversion from '_Ty' to '_Ty', possible loss of data
             # disable C4101: 'e': unreferenced local variable
-            target_compile_options(${Options_NAME} PRIVATE /W4 /wd4456 /wd4244 /wd4101 )
+            target_compile_options(${Options_NAME} PRIVATE /W4 /wd4456 /wd4244 /wd4101)
         endif()
 
         if(Options_OPTIMIZATION)
-            target_compile_options(${Options_NAME} PRIVATE $<IF:$<CONFIG:Debug>,/Od,/Ox>)
+            target_compile_options(${Options_NAME} PRIVATE $<IF:$<CONFIG:Debug>,/Od,/O2>)
         endif()
 
         if(Options_INTERFACE)
@@ -35,9 +35,9 @@ function(append_compilation_options)
             # This will disable `unexpected` in global namespace from eh.h
             target_compile_definitions(${Options_NAME} INTERFACE _HAS_CXX23)
         endif()
-    elseif(CMAKE_CXX_COMPILER_ID MATCHES "GNU" OR CMAKE_CXX_COMPILER_ID MATCHES "(Apple)?Clang")
+    elseif(CMAKE_CXX_COMPILER_ID MATCHES "GNU" OR CMAKE_CXX_COMPILER_ID MATCHES "Clang")
         if(Options_WARNINGS)
-            target_compile_options(${Options_NAME} PRIVATE -Wall -Wextra -Wpedantic -Werror -Wno-redundant-move)
+            target_compile_options(${Options_NAME} PRIVATE -Wall -Wextra -Wpedantic -Werror)
 
             if(CMAKE_CXX_COMPILER_ID MATCHES "Clang" AND CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL 19)
                 target_compile_options(${Options_NAME} PRIVATE -Wno-c2y-extensions)
@@ -45,20 +45,30 @@ function(append_compilation_options)
         endif()
 
         if(Options_OPTIMIZATION)
-            if(CMAKE_CXX_COMPILER_ID MATCHES "GNU" AND CMAKE_BUILD_TYPE STREQUAL "Debug")
-                target_compile_options(${Options_NAME} PRIVATE -O0 -fsanitize=address -static-libasan -fno-omit-frame-pointer)
-                target_link_options(${Options_NAME} PRIVATE -fsanitize=address)
-
-                if (NOT CMAKE_CXX_COMPILER_ID MATCHES "(Apple)?Clang")
-                    target_compile_options(${Options_NAME} PRIVATE -funreachable-traps)
-                endif()
+            # GCC 16.1 constexpr evaluator is incompatible with UBSan instrumentation in libstdc++
+            if(CMAKE_CXX_COMPILER_ID MATCHES "GNU" AND CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL 16)
+                set(sanitizers address,leak)
             else()
-                target_compile_options(${Options_NAME} PRIVATE $<IF:$<CONFIG:Debug>,-O0 -fno-omit-frame-pointer,-O2>)
+                set(sanitizers address,undefined,leak)
             endif()
+
+            target_compile_options(${Options_NAME} PRIVATE
+                $<$<CONFIG:Debug>:-O0>
+                $<$<CONFIG:Debug>:-fsanitize=${sanitizers}>
+                $<$<CONFIG:Debug>:-fno-omit-frame-pointer>
+                $<$<AND:$<CONFIG:Debug>,$<CXX_COMPILER_ID:GNU>>:-funreachable-traps>
+                $<$<NOT:$<CONFIG:Debug>>:-O2>
+            )
+
+            target_link_options(${Options_NAME} PRIVATE
+                $<$<CONFIG:Debug>:-fsanitize=${sanitizers}>
+            )
+
+            unset(sanitizers)
         endif()
 
         if(Options_INTERFACE)
-            if (NOT CMAKE_CXX_COMPILER_ID MATCHES "(Apple)?Clang")
+            if(CMAKE_CXX_COMPILER_ID MATCHES "GNU")
                 target_compile_options(${Options_NAME} INTERFACE -Wno-non-template-friend)
             endif()
 
@@ -68,21 +78,25 @@ function(append_compilation_options)
         if(Options_TESTS)
             target_compile_options(${Options_NAME} PRIVATE -fno-omit-frame-pointer)
 
+            # -Wno-redundant-move: want `std::move(std::as_const(x))` to be compiled without warnings in unit tests.
+            target_compile_options(${Options_NAME} PRIVATE -Wno-redundant-move)
+
             if(COVERAGE)
                 add_code_coverage_to_target(${Options_NAME} PRIVATE)
 
-                target_compile_options(${Options_NAME} PRIVATE
-                    -fno-inline-small-functions
-                    -fno-default-inline
-                    -fno-early-inlining
-                    -fno-aggressive-loop-optimizations
-                    -fno-peephole
-                    -fno-unit-at-a-time
-                    -fno-unroll-loops
-                )
+                # Some options may be redundant, but that's OK.
+                target_compile_options(${Options_NAME} PRIVATE -O0)
 
-                if (NOT CMAKE_CXX_COMPILER_ID MATCHES "(Apple)?Clang")
-                    target_compile_options(${Options_NAME} PRIVATE -funreachable-traps)
+                if(CMAKE_CXX_COMPILER_ID MATCHES "GNU")
+                    target_compile_options(${Options_NAME} PRIVATE
+                        -fno-inline-small-functions
+                        -fno-default-inline
+                        -fno-early-inlining
+                        -fno-aggressive-loop-optimizations
+                        -fno-peephole
+                        -fno-unroll-loops
+                        -funreachable-traps
+                    )
                 endif()
             endif()
         endif()
