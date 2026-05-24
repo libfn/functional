66def5e7e3461dbcba18a699a2d758def09f8c14
Bronek Kozicki <brok@incorrekt.com>
2026-05-24 18:45:10 +0100

    Fix Catch2 nix build issue

--- nix/catch2_3.nix
+++ nix/catch2_3.nix
@@ -20,6 +20,17 @@ stdenv.mkDerivation rec {
     cmake
   ];
 
+  # On Nix, @lib_dir@ and @include_dir@ already expand to absolute paths under
+  # /nix/store/..., so the upstream "${prefix}/@…@" join produces a double slash
+  # that fixupPhase rejects. See https://github.com/NixOS/nixpkgs/issues/144170.
+  postPatch = ''
+    substituteInPlace CMake/catch2.pc.in \
+      --replace 'libdir=''${prefix}/@lib_dir@' 'libdir=@lib_dir@' \
+      --replace 'includedir=''${prefix}/@include_dir@' 'includedir=@include_dir@'
+    substituteInPlace CMake/catch2-with-main.pc.in \
+      --replace 'libdir=''${prefix}/@lib_dir@' 'libdir=@lib_dir@'
+  '';
+
   hardeningDisable = [ "trivialautovarinit" ];
 
   cmakeFlags = [
