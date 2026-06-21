46ac318ba5915c65242373cf4d399443412cb982
Bronek Kozicki <brok@incorrekt.com>
2026-06-21 17:27:52 +0100

    Install node version 24

---
+++ ci/fetch-node.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+: "${NODE_RELEASE:?NODE_RELEASE is not set}"
+
+curl --proto '=https' --tlsv1.2 -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | \
+	gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+printf "%s\n%s\n" \
+	"deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_${NODE_RELEASE}.x nodistro main" \
+	"deb-src [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_${NODE_RELEASE}.x nodistro main" \
+	| tee /etc/apt/sources.list.d/nodesource.list
+printf "%s\n%s\n%s\n" \
+	"Package: nodejs" \
+	"Pin: origin deb.nodesource.com" \
+	"Pin-Priority: 600" \
+	| tee /etc/apt/preferences.d/nodejs
+apt-get update
+apt-get install -y --no-install-recommends nodejs
