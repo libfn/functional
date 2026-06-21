7332c8179ce308df6dbf017829901c27ac544254
Bronek Kozicki <brok@incorrekt.com>
2026-06-21 19:13:54 +0100

    Remove deb-src for https://deb.nodesource.com/node_${NODE_RELEASE}.x nodistro main

--- ci/fetch-node.sh
+++ ci/fetch-node.sh
@@ -8,7 +8,6 @@ curl --proto '=https' --tlsv1.2 -fsSL https://deb.nodesource.com/gpgkey/nodesour
   | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
 printf "%s\n%s\n" \
   "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_${NODE_RELEASE}.x nodistro main" \
-  "deb-src [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_${NODE_RELEASE}.x nodistro main" \
   | tee /etc/apt/sources.list.d/nodesource.list
 printf "%s\n%s\n%s\n" \
   "Package: nodejs" \
